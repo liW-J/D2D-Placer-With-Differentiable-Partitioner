b658331faa3a5b80670cb16d5065c3a7cdf1e82f
JeanneWillis <hi@jeannewillis.cn>
2025-03-14 18:57:11 +0800

    link DREAMPlace in CMake

---
+++ src/Parser.py
@@ -0,0 +1,10 @@
+'''
+Author: JeanneWillis hi@jeannewillis.cn
+Date: 2025-03-13 20:53:37
+LastEditors: JeanneWillis hi@jeannewillis.cn
+LastEditTime: 2025-03-13 21:04:22
+FilePath: /D2D-placer/src/Parser.py
+Description: Parser ICCAD-2022 benchmark
+'''
+
+
