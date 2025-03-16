001d8e9ba9e4e971d5c1ee0e1055ed3d2f9a972f
JeanneWillis <hi@jeannewillis.cn>
2025-03-16 23:05:05 +0800

    parser iccad-txt by 3d-placer

--- src/ops/parser_txt/parser_txt.py
+++ src/ops/parser_txt/parser_txt.py
@@ -2,7 +2,7 @@
 Author: JeanneWillis hi@jeannewillis.cn
 Date: 2025-03-15 14:13:35
 LastEditors: JeanneWillis hi@jeannewillis.cn
-LastEditTime: 2025-03-16 00:55:50
+LastEditTime: 2025-03-16 22:34:27
 FilePath: /D2D-placer/src/ops/parser_txt/parser_txt.py
 Description: parser ICCAD 3D placement txt file
 '''
@@ -17,20 +17,23 @@ import parser_txt_cpp
 
 class ParserTxtFunction(Function):
     @staticmethod
-    def forward(ctx, file_path):
+    def forward(file_path, output_path):
         
         func = parser_txt_cpp.parser_txt
-        output = func(file_path)
+        input_args = ["3d-placer", file_path, output_path]
+        output = func(input_args)
         
         return output
     
 class ParserTxt(object):
-    def __init__(self, file_path):
+    def __init__(self, file_path, output_path):
         self.file_path = file_path
+        self.output_path = output_path
         
     def __call__(self):
-        return ParserTxtFunction.forward(self.file_path)
+        return ParserTxtFunction.forward(self.file_path, self.output_path)
 
 if __name__ == "__main__":
-    parser_txt = ParserTxt("/D2D-placer/benchmark/iccad2022/case1.txt")
+    parser_txt = ParserTxt("/D2D-placer/benchmarks/iccad2022/case1.txt", "/D2D-placer/output/case1_output.txt")
+    result = parser_txt()
     print("ok!")
--- src/ops/parser_txt/src/parser_txt.cpp
+++ src/ops/parser_txt/src/parser_txt.cpp
@@ -2,32 +2,47 @@
  * @Author: JeanneWillis hi@jeannewillis.cn
  * @Date: 2025-03-15 14:41:38
  * @LastEditors: JeanneWillis hi@jeannewillis.cn
- * @LastEditTime: 2025-03-16 20:21:48
+ * @LastEditTime: 2025-03-16 23:03:10
  * @FilePath: /D2D-placer/src/ops/read_txt/src/read_txt.cpp
  * @Description: 
  */
 
-// dreamplace database dependency
-#include "dreamplace/ops/place_io/src/PyPlaceDB.h"
+#include <pybind11/pybind11.h>
 // 3d-placer parser
+#include "utility/paramHdl.h"
 #include "parser/parser.h"
-
+#include "dataModel/dm.h"
+#include "placer/placer.h"
 #include "include/common.h"
 
 PLACER_BEGIN_NAMESPACE
 
-DREAMPLACE_NAMESPACE::PlaceDB parser_txt_forward(string fileName)
+int parser_txt_forward(pybind11::list const& args)
 {
-  DREAMPLACE_NAMESPACE::PlaceDB db; 
+  clock_t tStart = clock();
+  // args -> argc, argv
+  int argc = pybind11::len(args); 
+  char** argv = new char* [argc]; 
+  for (int i = 0; i < argc; ++i)
+  {
+      std::string token = pybind11::str(args[i]); 
+      argv[i] = new char [token.size()+1];
+      std::copy(token.begin(), token.end(), argv[i]); 
+      argv[i][token.size()] = '\0';
+  }
 
   // txt2bookself
+  ParamHdl_C paramHdl = ParamHdl_C(argc, argv);
   Parser_C parser;
-  if(!parser.read_file(fileName))
-    return db;
-  return db;
+  parser.read_file(paramHdl.get_input_fileName());
+  DmMgr_C* dmMgr = new DmMgr_C(parser, paramHdl, tStart);
+  dmMgr->print_info();
+  dmMgr->txt2bookself();
+  return 0;
 }
 
 PLACER_END_NAMESPACE
+
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("parser_txt", &PLACER_NAMESPACE::parser_txt_forward, "parser_txt_forward");
